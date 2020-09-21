a17cddb9c10602922549617e11ba001c5c3089cc
Christopher-DeLaTorre <69987322+Christopher-DeLaTorre@users.noreply.github.com>
2020-09-21 09:30:40 -0700

    Add files via upload

--- functions.py
+++ functions.py
@@ -4,4 +4,25 @@ def area(width, height):
 result = area(5, 6)
 #print(result)
 call1 = area(6, 6)
-print(call1)
+call2 = area(9, 9)
+
+def subtract(num1, num2):
+    result = num1 - num2
+    return result
+sub1 = subtract(6, 2)
+sub2 = subtract(4, 4)
+print(sub1)
+print(sub2)
+
+def divide(in1, in2):
+    result = in1/in2
+    return result
+
+def add(n1, n2):
+    result = n1 + n2
+    return result
+
+add1 = add(1, 1)
+add2 = add(2, 2)
+print(add1)
+print(add2)
